e25ab45ba90e2c1d6f8b2e8e29ca036fe6022384
darkhankz <upstudynow@gmail.com>
2022-09-04 18:11:08 +0600

    CleanArchToEnd

--- app/src/main/java/com/movies/mymoviesapp/presentaion/main/MainFragment.kt
+++ app/src/main/java/com/movies/mymoviesapp/presentaion/main/MainFragment.kt
@@ -5,56 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.movies.mymoviesapp.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import androidx.recyclerview.widget.RecyclerView
+import com.movies.mymoviesapp.databinding.FragmentMainBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _mMainBinding: FragmentMainBinding ? = null
+    private val mBinding get() = _mMainBinding!!
+
+    private lateinit var recyclerView: RecyclerView
+    private val mAdapter by lazy { MainFragmentAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+       _mMainBinding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
     }
+
+    private fun init() {
+        val viewModel = ViewModelProvider(this)[MainFragmentViewModel::class.java]
+        recyclerView = mBinding.rvMain
+        recyclerView.adapter = mAdapter
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _mMainBinding = null
+    }
+
+
 }
