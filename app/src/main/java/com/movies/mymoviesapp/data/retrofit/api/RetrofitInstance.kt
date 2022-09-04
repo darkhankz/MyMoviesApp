a894b7aba0bc23bdc92360ec4b4ccbff93ea4895
darkhankz <upstudynow@gmail.com>
2022-09-04 20:28:59 +0600

    CleanArchToEnd

--- app/src/main/java/com/movies/mymoviesapp/data/retrofit/api/RetrofitInstance.kt
+++ app/src/main/java/com/movies/mymoviesapp/data/retrofit/api/RetrofitInstance.kt
@@ -1,4 +1,19 @@
 package com.movies.mymoviesapp.data.retrofit.api
 
+import com.movies.mymoviesapp.common.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
 object RetrofitInstance {
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val api: ApiInterface by lazy {
+        retrofit.create(ApiInterface::class.java)
+    }
 }
--- app/src/main/java/com/movies/mymoviesapp/presentaion/main/MainFragmentAdapter.kt
+++ app/src/main/java/com/movies/mymoviesapp/presentaion/main/MainFragmentAdapter.kt
@@ -1,4 +1,38 @@
 package com.movies.mymoviesapp.presentaion.main
 
-class MainFragmentAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.movies.mymoviesapp.R
+import com.movies.mymoviesapp.common.MAIN
+import com.movies.mymoviesapp.domain.models.Result
+import kotlinx.android.synthetic.main.item_card.view.*
+
+class MainFragmentAdapter: RecyclerView.Adapter<MainFragmentAdapter.ViewHolder>() {
+    private var listMovies = emptyList<Result>()
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.item_title_card.text = listMovies[position].title
+        holder.itemView.item_date_card.text = listMovies[position].release_date
+        Glide
+            .with(MAIN)
+            .load(listMovies[position].poster_path)
+            .centerCrop()
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .into(holder.itemView.item_image_card)
+
+    }
+
+    override fun getItemCount(): Int {
+        return listMovies.size
+    }
+
 }
